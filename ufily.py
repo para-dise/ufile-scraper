aa7349ba434a49b67b009be17e60fa58f06e027a
paradise <xkcocco@yahoo.com>
2019-04-01 09:38:13 +0100

    Update ufily.py
    Added threading, speeds are improved now.

--- ufily.py
+++ ufily.py
@@ -4,18 +4,30 @@ import requests
 import time
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+from threading import Thread
+
+gay = []
 
 def ran(size=5, chars=string.ascii_letters + string.digits):
-    return ''.join(random.choice(chars) for x in range(size))
+    e = ''.join(random.choice(chars) for x in range(size))
+    if e not in gay:
+      return e
+    gay.append(e)
+
+def scrapefile():
+  time.sleep(.1)
+  link = ran()
+  fileurl = 'https://217.182.136.95/' + link
+  asd = requests.get(fileurl, verify=False)
+  if asd.status_code == 200 and "Sorry it's gone..." not in asd.text:
+    print('\33[32mValid link: '+ fileurl + "\33[0m")
+    f = open("working.txt", "a")
+    f.write(fileurl + "\n")
+    f.close()
+  else:
+    pass
+
 
 for i in range(1,10000000):
-    time.sleep(.1)
-    link = ran()
-    
-    fileurl = 'https://217.182.136.95/'+link
-    asd = requests.get(fileurl, verify=False)
-    #print(asd)
-    if asd.status_code == 200:
-          print('attempted code: '+fileurl)
-    else:
-          pass
+  t = Thread(target=scrapefile)
+  t.start()

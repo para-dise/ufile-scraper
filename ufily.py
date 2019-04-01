9178ab9d76389ca154e2b6df4b8d04f34339b917
paradise <xkcocco@yahoo.com>
2019-04-01 10:14:00 +0100

    Update ufily.py

--- ufily.py
+++ ufily.py
@@ -5,6 +5,8 @@ import time
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 from threading import Thread
+import bs4
+
 
 gay = []
 
@@ -20,10 +22,12 @@ def scrapefile():
   fileurl = 'https://217.182.136.95/' + link
   asd = requests.get(fileurl, verify=False)
   if asd.status_code == 200 and "Sorry it's gone..." not in asd.text:
-    print('\33[32mValid link: '+ fileurl + "\33[0m")
-    f = open("working.txt", "a")
-    f.write(fileurl + "\n")
-    f.close()
+    html = bs4.BeautifulSoup(asd.text)
+    h = html.title.text.replace("Uploadfiles.io - Download ", "")
+    h = h.replace("for free", "")
+    print('\33[32mValid link: '+ fileurl.replace("217.182.136.95", "ufile.io") + " | " + h + "\33[0m")
+    with open("working.txt", "a") as f:
+      f.write(fileurl + "\n")
   else:
     pass
 
